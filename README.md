c0effcf21878809feb0c439b19c2c4a69658c6ec
Paul  Ranson <paulranson@Pauls-MacBook-Air.local>
2024-12-01 18:17:11 +0000

    Skeleton for aco2

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Advent Of Code 2024
 
 aoc_test contains some old solutions from previous years and isn't pertinent.
 
-Each new day of 2024 will get it's own directory. Builds should be good on MacOS, Windows and Linux.
+Each new day of 2024 will get its own directory. Builds should be good on MacOS, Windows and Linux.
 
 For reference,
 
---
+++ aoc2/aoc2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+
+#include "ctre_inc.h"
+
+auto get_input()
+{
+
+}
+
+int64_t pt1()
+{
+	return 0;
+}
+
+int64_t pt2()
+{
+	return 0;
+}
+
+int main()
+{
+	std::cout << "pt1 = " << pt1() << "\n";
+	std::cout << "pt2 = " << pt2() << "\n";
+}
