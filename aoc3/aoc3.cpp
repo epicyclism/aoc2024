6e04ee01cd0adf4dfb8cb9f4c2fb05c8a28be9c8
Paul <paul@epicyclism.com>
2024-12-03 09:23:23 +0000

    simplfy input aoc3

--- aoc3/aoc3.cpp
+++ aoc3/aoc3.cpp
@@ -3,37 +3,37 @@
 #include <string>
 
 #include "ctre_inc.h"
+#include "timer.h"
 
 auto get_input()
 {
-	std::vector<std::string> v;
+	std::string r;
 	std::string ln;
 	while(std::getline(std::cin, ln))
-		v.emplace_back(ln);
-	return v;
+		r += ln;
+	return r;
 }
 
 auto pt12(auto const& in)
 {
+	timer t("p12");
 	bool do_{true};
 	int64_t p1{0};
 	int64_t p2{0};
-	for(auto& s: in)
-//		for (auto [m, a, b, d1, d0] : ctre::search_all<"mul\\((\\d+),(\\d+)\\)|(do\\(\\))|(don't\\(\\))">(s))
-		for (auto [m, a, b, d1, d0] : ctre::search_all<"mul\\((\\d{1,3}),(\\d{1,3})\\)|(do\\(\\))|(don't\\(\\))">(s))
+	for (auto [m, a, b, d1, d0] : ctre::search_all<"mul\\((\\d{1,3}),(\\d{1,3})\\)|(do\\(\\))|(don't\\(\\))">(in))
+	{
+		if(d1)
+			do_ = true;
+		else if(d0)
+			do_ = false;
+		else
 		{
-			if(d1)
-				do_ = true;
-			else if(d0)
-				do_ = false;
-			else
-			{
-				auto p = a.template to_number<int>() * b.template to_number<int>();
-				p1 += p;
-				if(do_)
-					p2 += p;
-			}
+			auto p = a.template to_number<int>() * b.template to_number<int>();
+			p1 += p;
+			if(do_)
+				p2 += p;
 		}
+	}
 
 	return std::make_pair(p1, p2);
 }
