bae622eeda11a3ed767095030fcbc3eeb38f24af
Paul Ranson <paul@epicyclism.com>
2024-12-05 17:43:40 +0000

    2Tidy

--- aoc5/aoc5.cpp
+++ aoc5/aoc5.cpp
@@ -5,7 +5,6 @@
 #include <bitset>
 
 #include "ctre_inc.h"
-#include "linear_set.h"
 #include "timer.h"
 
 auto get_input()
@@ -37,39 +36,12 @@ bool ordered (auto const& rules, auto const& l)
 	for(int n{0}; n < l.size(); ++n)
 	{
 		for(int m{n + 1}; m < l.size(); ++m)
-		{
-			if(l[n] > 99)
-				std::cout << l[n] << " oops\n";
 			if(rules[l[n]].test(l[m]))
 				return false;
-		}
 	}
 	return true;
 }
 
-#if 0
-std::pair<int, int> pt12o(auto const& rules, auto& lists)
-{
-	timer t("pt12o");
-	int rv1{0};
-	int rv2{0};
-
-	for(auto& l: lists)
-	{
-		if(ordered(rules, l))
-			rv1 += l[l.size() / 2];
-		else
-		{
-			std::sort(l.begin(), l.end(), [&](auto le, auto re)
-				{
-					return !rules[re].test(le);
-				});
-			rv2 += l[l.size() / 2];
-		}
-	}
-	return std::make_pair(rv1, rv2);
-}
-#endif
 std::pair<int, int> pt12(auto const& rules, auto const& lists)
 {
 	timer t("pt12");
@@ -101,9 +73,6 @@ std::pair<int, int> pt12(auto const& rules, auto const& lists)
 int main()
 {
 	auto [rules, lists] = get_input();
-//	auto[p1, p2] = pt12o(rules, lists);
-//	std::cout << "pt1 = " << p1 << "\n";
-//	std::cout << "pt2 = " << p2 << "\n";
 	auto[p1, p2] = pt12(rules, lists);
 	std::cout << "pt1 = " << p1 << "\n";
 	std::cout << "pt2 = " << p2 << "\n";
