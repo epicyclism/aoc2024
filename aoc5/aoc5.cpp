a9f31fc5d27341970bedfb01599a51a57e72472c
Paul Ranson <paul@epicyclism.com>
2024-12-05 16:18:46 +0000

    aoc5 to bitset rather than array

--- aoc5/aoc5.cpp
+++ aoc5/aoc5.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <bitset>
 
 #include "ctre_inc.h"
 #include "linear_set.h"
@@ -9,7 +10,7 @@
 
 auto get_input()
 {
-	std::vector<linear_set<unsigned char>> rules(100);
+	std::vector<std::bitset<100>> rules(100);
 	std::vector<std::vector<unsigned char>> lists;
 	std::string ln;
 	while(std::getline(std::cin, ln))
@@ -18,7 +19,7 @@ auto get_input()
 			break;
 		if(auto[m, a, b] = ctre::match<"(\\d+)\\|(\\d+)">(ln); m)
 		{
-			rules[b.to_number<int>()].emplace_back( a.to_number<unsigned char>());
+			rules[b.to_number<int>()].set(a.to_number<unsigned char>());
 		}
 	}
 	while(std::getline(std::cin, ln))
@@ -39,7 +40,7 @@ bool ordered (auto const& rules, auto const& l)
 		{
 			if(l[n] > 99)
 				std::cout << l[n] << " oops\n";
-			if(rules[l[n]].contains(l[m]))
+			if(rules[l[n]].test(l[m]))
 				return false;
 		}
 	}
@@ -61,9 +62,39 @@ std::pair<int, int> pt12(auto const& rules, auto& lists)
 			std::sort(l.begin(), l.end(), [&](auto le, auto re)
 //			std::nth_element(l.begin(), l.begin() + l.size() / 2, l.end(), [&](auto le, auto re)
 				{
-					return !rules[re].contains(le);
+					return !rules[re].test(le);
 				});
 			rv2 += l[l.size() / 2];
+		}
+	}
+	return std::make_pair(rv1, rv2);
+}
+
+std::pair<int, int> pt12b(auto const& rules, auto& lists)
+{
+	timer t("pt12b");
+	int rv1{ 0 };
+	int rv2{ 0 };
+
+	for (auto& l : lists)
+	{
+		if (ordered(rules, l))
+			rv1 += l[l.size() / 2];
+		else
+		{
+			std::bitset<100> bsl;
+			for (auto e : l)
+				bsl.set(e);
+			for (auto e : l)
+			{
+				bsl.set(e, false);
+				if((rules[e] & bsl).count() == l.size() / 2)
+				{
+					rv2 += e;
+					break;
+				}
+				bsl.set(e);
+			}
 		}
 	}
 	return std::make_pair(rv1, rv2);
@@ -75,4 +106,7 @@ int main()
 	auto[p1, p2] = pt12(rules, lists);
 	std::cout << "pt1 = " << p1 << "\n";
 	std::cout << "pt2 = " << p2 << "\n";
+	auto[p1t, p2t] = pt12b(rules, lists);
+	std::cout << "pt1 = " << p1t << "\n";
+	std::cout << "pt2 = " << p2t << "\n";
 }
