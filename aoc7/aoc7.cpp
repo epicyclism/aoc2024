218a82dc2a127191e45655fde32dd59d1e0ee28a
Paul <paul@epicyclism.com>
2024-12-07 09:21:55 +0000

    ooc7

--- aoc7/aoc7.cpp
+++ aoc7/aoc7.cpp
@@ -3,50 +3,138 @@
 #include <string>
 #include <algorithm>
 #include <numeric>
-#include <ranges>
 
 #include "ctre_inc.h"
+#include "timer.h"
+
+struct equation
+{
+	uint64_t val_;
+	std::vector<uint64_t> pts_;
+};
 
 auto get_input()
 {
-	std::vector<int64_t> vl;
-	std::vector<int64_t> vr;
+	std::vector<equation> rv;
 	std::string ln;
 	while(std::getline(std::cin, ln))
 	{
-		if(auto[m, a, b] = ctre::match<"(\\d+)\\s+(\\d+)">(ln); m)
+		equation e;
+		bool p{false};
+		for(auto v: ctre::search_all<"(\\d+)">(ln))
 		{
-			vl.emplace_back(a.to_number<int64_t>());
-			vr.emplace_back(b.to_number<int64_t>());
+			if(p)
+				e.pts_.emplace_back(v.to_number<uint64_t>());
+			else
+				e.val_ = v.to_number<uint64_t>();
+			p = true;
 		}
+		rv.emplace_back(e);
 	}
-	std::ranges::sort(vl);
-	std::ranges::sort(vr);
 
-	return std::make_pair(vl, vr);
+	return rv;
 }
 
-int64_t pt1(auto const& l, auto const& r)
+int64_t eval(std::vector<uint64_t> const& val, uint64_t ops)
 {
-	return std::transform_reduce(l.begin(), l.end(), r.begin(), 0LL, std::plus<>(), [](auto l, auto r)
+	return std::accumulate(val.begin() + 1, val.end(), val.front(), [&](auto a, auto& b)
 		{
-			return std::abs(l - r);
+			if(ops & 1)
+				a *= b;
+			else
+				a += b;
+			ops >>= 1;
+			return a;
 		});
 }
 
-int64_t pt2(auto const& vl, auto const& vr)
+uint64_t ashift(uint64_t a, uint64_t b)
 {
-	int64_t v { 0};
-	for(auto l: vl)
+	while(b)
 	{
-		v += l * std::count(vr.begin(), vr.end(), l);
+		a*=10;
+		b/=10;
 	}
-	return v;
+	return a;
+}
+
+uint64_t eval2(std::vector<uint64_t> const& val, uint64_t ops)
+{
+	return std::accumulate(val.begin() + 1, val.end(), val.front(), [&](auto a, auto& b)
+		{
+			switch(ops & 0x03)
+			{
+				default:
+				case 0:
+					a += b;
+					break;
+				case 1:
+					a *= b;
+					break;
+				case 2:
+					a = ashift(a, b) + b;
+					break;
+			}
+			ops >>= 2;
+			return a;
+		});
+}
+
+bool can_be(equation const& e)
+{
+	uint64_t lim { (1ULL << (e.pts_.size() - 1)) - 1};
+	for(uint64_t op { 0 }; op <= lim; ++op)
+		if(eval(e.pts_, op) == e.val_)
+			return true;
+	return false;
+}
+
+bool valid(uint64_t op)
+{
+	while(op)
+	{
+		if((op & 0x03) == 0x03)
+			return false;
+		op >>= 2;
+	}
+	return true;
+}
+
+bool can_be2(equation const& e)
+{
+	uint64_t lim { 1ULL << ((e.pts_.size() - 1) * 2)};
+	--lim;
+	for(uint64_t op { 0 }; op <= lim; ++op)
+	{
+		if( valid(op) && eval2(e.pts_, op) == e.val_)
+			return true;
+	}
+	return false;
+}
+
+int64_t pt1(auto const& in)
+{
+	timer t ("p1");
+	uint64_t r{0};
+	for(auto& e: in)
+		if(can_be(e))
+			r += e.val_;
+	return r;
+}
+
+int64_t pt2(auto const& in)
+{
+	timer t ("p2");
+	uint64_t r{0};
+	for(auto& e: in)
+		if(can_be2(e))
+			r += e.val_;
+	return r;
 }
 
 int main()
 {
-	auto [l, r] = get_input();
-	std::cout << "pt1 = " << pt1(l, r) << "\n";
-	std::cout << "pt2 = " << pt2(l, r) << "\n";
+	auto in = get_input();
+	std::cout << "pt1 = " << pt1(in) << "\n";
+	std::cout << "pt2 = " << pt2(in) << "\n";
 }
